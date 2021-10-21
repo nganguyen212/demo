177c7f6042c9d4131873e97bc93c3e7250a453ab
thuongnguyen01685 <sinanhsinanh@gmail.com>
2021-10-21 07:56:24 +0700

    PhanvungAdmin

---
+++ tntnews/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tntnews.Areas.Admin.Code;
+
+namespace tntnews.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            if (SessionHelper.GetSession() != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login", new { Area = "Admin" });
+            }
+
+        }
+    }
+}
