92e638c8c1cbde38be9cc9b911e06476c25ab577
Nguyen Thi Nga <nganguyen20102k@gmail.com>
2021-10-22 20:00:20 +0700

    add project

--- Models/DAO/PostDb.cs
+++ Models/DAO/PostDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,45 @@ using System.Threading.Tasks;
 
 namespace Models.DAO
 {
-    class PostDb
+    public class PostDb
     {
+        DtbContext context = null;
+        public PostDb()
+        {
+            context = new DtbContext();
+        }
+        public IEnumerable<post> GetPosts()
+        {
+            var result = context.posts.OrderByDescending(x => x.title).ToList();
+            return result;
+        }
+        public void Insert<T>(T model) where T : class
+        {
+            using (var ct = new DtbContext())
+            {
+               ct.Set<T>().Add(model);
+               ct.SaveChanges();
+
+            }
+        }
+        public bool UpdatePost(post po)
+        {
+            try
+            {
+
+                var p = context.posts.Find(po.id);
+                p.title=po.title;
+                p.description = po.description;
+                p.publish_state = false;
+                context.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
     }
 }
---
+++ tntnews/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,74 @@
+﻿using Models.DAO;
+using Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tntnews.Areas.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Admin/Post
+        public ActionResult Index()
+        {
+            var post = new PostDb();
+            var model = post.GetPosts();
+            return View(model);
+           
+        }
+        public ActionResult CreatePost()
+        {
+            return View();
+        }
+        public ActionResult HienThiPost()
+        {
+            var post = new PostDb();
+            var model = post.GetPosts();
+            return View(model);
+        }
+        public ActionResult CreatePost(post model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new PostDb();
+                var po = new post();
+
+                po.title = model.title;
+
+                if (po != null)
+                {
+                    dao.Insert<post>(po);
+                    return RedirectToAction("index");
+
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Thêm không thành công");
+            }
+            return View();
+        }
+        public ActionResult UpdatePost()
+        {
+
+
+            return View();
+        }
+        public ActionResult UpdatePost(post model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new PostDb();
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "Cập nhật không thành công");
+            }
+            return View();
+        }
+
+    }
+}
